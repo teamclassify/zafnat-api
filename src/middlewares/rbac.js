4f1db7acadab04ddbe61486315e104b771a99d02
Andres Parra <byandrev@gmail.com>
2024-10-21 11:17:33 -0500

    Refactor rbac middleware to use role constants for permission checks

--- src/middlewares/rbac.js
+++ src/middlewares/rbac.js
@@ -1,8 +1,14 @@
+const ROLES = {
+  ADMIN: 0,
+  SALES: 1,
+  USER: 2,
+};
+
 function checkPermission(role) {
   return (req, res, next) => {
-    const userRole = req.user ? req.user.role : "anonymous";
+    const userRole = req.user ? req.user.roles : "anonymous";
 
-    if (userRole.includes(role)) {
+    if (userRole.includes(ROLES[role])) {
       return next();
     } else {
       return res.status(403).json({ error: "Access denied" });
