3e1f3b492182af320a9a2e8fc0ea032ec4c41d8f
anderssonccg <anderssonccg@gmail.com>
2024-10-22 10:43:22 -0500

    feat: add new validation for admin on UserRouter

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import UserController from "../controllers/UserController.js";
 import verifyToken from "../middlewares/verifyToken.js";
+import onlyAdmin from "../middlewares/onlyAdmin.js";
 
 const userRouter = express.Router();
 const controller = new UserController();
 
-userRouter.get("/", verifyToken, controller.findAll);
+userRouter.get("/", verifyToken, onlyAdmin, controller.findAll);
 userRouter.get("/:id", verifyToken, controller.findOne);
 
 export default userRouter;
