3e3b580c9926a731e99bfbcc8f455e53a3c1582f
anderssonccg <anderssonccg@gmail.com>
2024-10-22 10:36:50 -0500

    feat: add verification for admin

---
+++ src/utils/verifyIsAdmin.js
@@ -0,0 +1,14 @@
+import UsersOnRoles from "../services/UsersOnRolesService.js";
+
+const verifyIsAdmin = async (req, res) => {
+  const usersOnRolesService = new UsersOnRoles();
+
+  const user = await usersOnRolesService.find({
+    userId: req.uid,
+    roleId: 1,
+  });
+
+  return user.length > 0;
+}
+
+export default verifyIsAdmin;
