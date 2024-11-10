8434c49a71b53305d6333ef36410e4d62be5dca0
Andres Parra <byandrev@gmail.com>
2024-11-09 20:06:00 -0500

    feat: only allow update roles to admins

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import UserController from "../controllers/UserController.js";
 import verifyToken from "../middlewares/verifyToken.js";
+import checkPermission from "../middlewares/rbac.js";
 
 const userRouter = express.Router();
 const controller = new UserController();
@@ -8,8 +9,8 @@ const controller = new UserController();
 userRouter.get("/", verifyToken, controller.findAll);
 userRouter.get("/roles", verifyToken, controller.getRoles);
 userRouter.get("/:id", verifyToken, controller.findOne);
-userRouter.post("/set-role", verifyToken, controller.setRole);
+userRouter.post("/set-role", verifyToken, checkPermission("ADMIN"), controller.setRole);
 userRouter.put("/update-profile", verifyToken, controller.update);
-userRouter.delete("/unset-role", verifyToken, controller.unsetRole);
+userRouter.delete("/unset-role", verifyToken, checkPermission("ADMIN"), controller.unsetRole);
 
 export default userRouter;
