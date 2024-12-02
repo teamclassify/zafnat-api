15e6e974f8de4c9a47c0a656a4da924e3fd7d443
Andres Parra <byandrev@gmail.com>
2024-12-01 20:02:46 -0500

    feat: create products sku

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -156,6 +156,133 @@ class ProductController {
 
     return res.json(dataResponse);
   }
+  
+  async updateSku(req, res) {
+    const id = req.params.id;
+    const {
+      id: skuId,
+      sku,
+      price,
+      images,
+      color_attribute_id,
+      size_attribute_id
+    } = req.body;
+    
+    if (!sku || !price) {
+      const data = new ResponseDataBuilder()
+        .setData(null)
+        .setStatus(400)
+        .setMsg("All fields are required")
+        .build();
+
+      return res.json(data);
+    }
+    
+    const productUpdated = await prisma.productSku.update({
+      where: {
+        id: parseInt(skuId),
+      },
+      data: {
+        sku,
+        price: parseFloat(price),
+        color_attribute_id,
+        size_attribute_id
+      }
+    })
+
+    if (!productUpdated) {
+      const data = new ResponseDataBuilder()
+        .setData(null)
+        .setStatus(404)
+        .setMsg("Product not found")
+        .build();
+
+      return res.json(data);
+    }
+
+    const dataResponse = new ResponseDataBuilder()
+      .setData(productUpdated)
+      .setStatus(200)
+      .setMsg("Product updated")
+      .build();
+
+    return res.json(dataResponse);
+  }
+  
+  async createSku(req, res) {
+    const {
+      productId,
+      sku,
+      price,
+      color,
+      size
+    } = req.body;
+
+    const productCreated = await prisma.productSku.create({
+      data: {
+        sku,
+        price: parseFloat(price),
+        
+        product: {
+          connect: {
+            id: parseInt(productId)
+          }
+        },
+        
+        color_attribute: {
+          connect: {
+            id: parseInt(color)
+          }
+        },
+        size_attribute: {
+          connect: {
+            id: parseInt(size)
+          }
+        },
+        quantity: 0
+      }
+    });
+
+    const dataResponse = new ResponseDataBuilder()
+      .setData(productCreated)
+      .setStatus(201)
+      .setMsg("Product created")
+      .build();
+
+    return res.json(dataResponse);
+  }
+  
+  async getColors(req, res) {
+    const colors = await prisma.productAttribute.findMany({
+      where: {
+        type: "color"
+      }
+    });
+
+    const data = new ResponseDataBuilder()
+      .setData(colors)
+      .setStatus(200)
+      .setMsg("Colors found")
+      .build();
+
+    return res.json(data);
+  }
+  
+  async getSizes(req, res) {
+    const sizes = await prisma.productAttribute.findMany({
+      where: {
+        type: "size"
+      }
+    });
+
+    const data = new ResponseDataBuilder()
+      .setData(sizes)
+      .setStatus(200)
+      .setMsg("Sizes found")
+      .build();
+
+    return res.json(data);
+  }
 }
 
 export default ProductController;
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -6,10 +6,16 @@ import checkPermission from "../middlewares/rbac.js";
 const productRouter = express.Router();
 const controller = new ProductController();
 
+productRouter.get("/colors", controller.getColors);
+productRouter.get("/sizes", controller.getSizes);
 productRouter.get("/",  controller.findAll);
 productRouter.get("/:id",controller.findOne);
 productRouter.post("/", verifyToken, checkPermission("ADMIN"), controller.create);
 productRouter.delete("/:id",verifyToken,checkPermission("ADMIN"),  controller.delete);
 productRouter.put("/:id", verifyToken, checkPermission("ADMIN"), controller.update);
+productRouter.put("/:id/sku", verifyToken, checkPermission("ADMIN"), controller.updateSku);
+productRouter.post("/:id/sku", verifyToken, checkPermission("ADMIN"), controller.createSku);
+
+
 
 export default productRouter;
