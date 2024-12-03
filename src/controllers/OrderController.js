2ea28c28bc724004d9ea79698a64bf4a0b6432eb
MauricioDDS <mauriciodidonato794@gmail.com>
2024-12-03 13:13:52 -0500

    fix: get orders by user_id

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,174 +1,184 @@
+import { MerchantOrder, Preference } from "mercadopago";
+import { URL_FRONT } from "../config/index.js";
+import client from "../config/mercadopago.js";
+import prisma from "../config/prisma.js";
+import OrderItemService from "../services/OrderItemService.js";
 import OrderService from "../services/OrderService.js";
 import ProductSkuService from "../services/ProductSkuService.js";
-import OrderItemService from "../services/OrderItemService.js";
-import { Preference, MerchantOrder, Payment } from "mercadopago";
-import client from "../config/mercadopago.js";
-import {URL_BACK, URL_FRONT} from "../config/index.js";
-import prisma from "../config/prisma.js";
 
 class OrderController {
-    constructor() {
-        this.orderService = new OrderService();
-        this.productSkuService = new ProductSkuService();
-        this.orderItemService = new OrderItemService();
+  constructor() {
+    this.orderService = new OrderService();
+    this.productSkuService = new ProductSkuService();
+    this.orderItemService = new OrderItemService();
+  }
+
+  findAll = async (req, res) => {
+    const orders = await this.orderService.find({});
+    res.json(orders);
+  };
+
+  findById = async (req, res) => {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+
+    if (!order) {
+      return res.status(404).json({ msg: "Order not found" });
     }
+    res.json(order);
+  };
 
-    findAll = async (req, res) => {
-        const orders = await this.orderService.find({});
-        res.json(orders);
+  findByUser = async (req, res) => {
+    const orders = await this.orderService.find({
+      user: {
+        id: req.params.user_id,
+      },
+    });
+    res.json(orders);
+  };
+
+  create = async (req, res) => {
+    if (!req.body.order_items || req.body.order_items.length === 0) {
+      return res.status(400).json({ msg: "Order items are required" });
     }
-    
-    findById = async (req, res) => {
-        const order = await prisma.order.findUnique({
-            where: {
-                id: parseInt(req.params.id)
-            }
-        })
-      
-        if (!order) {
-            return res.status(404).json({ msg: "Order not found" });
-        }
-        res.json(order);
+    if (!req.body.address_id) {
+      return res.status(400).json({ msg: "Address is required" });
     }
+    const { user_id, address_id, is_wholesale, order_items } = req.body;
 
-    findByUser = async (req, res) => {
-        const orders = await this.orderService.find({
-            where: {
-                user: {
-                    id: req.body.user_id,
-                },
-            },
-        });
-        res.json(orders);
-    };
+    const validate = await this.validateStock(order_items);
+    if (!validate) return res.status(400).json({ msg: "Insufficient stock" });
 
-    create = async (req, res) => {
-        if(!req.body.order_items || req.body.order_items.length === 0) {
-            return res.status(400).json({ msg: "Order items are required" });
-        }
-        if(!req.body.address_id) {
-            return res.status(400).json({ msg: "Address is required" });
-        }
-        const { user_id, address_id, is_wholesale, order_items } = req.body;
+    // console.log(order_items)
 
-        const validate = await this.validateStock(order_items);
-        if(!validate) return res.status(400).json({ msg: "Insufficient stock" });
-        
-        // console.log(order_items)
-        
-        let total = 0.0;
-        for (const item of order_items) {
-            total += item.product_sku.price * item.quantity;
-        }
-        const order = await this.orderService.create({
-            address_id,
-            user_id,
-            status: "waiting_shipment",
-            is_wholesale: false,
-            total: total,
-        });
-        
-        const invoice = await prisma.invoice.create({
-            data: {
-                order_id: order.id,
-                amount: total,
-                status: "paid",
-                transaction_id: "test",
-                bank: "mercadopago"
-            }
-        })
-        
-       const preference = new Preference(client);
+    let total = 0.0;
+    for (const item of order_items) {
+      total += item.product_sku.price * item.quantity;
+    }
+    const order = await this.orderService.create({
+      address_id,
+      user_id,
+      status: "waiting_shipment",
+      is_wholesale: false,
+      total: total,
+    });
 
-        //crear preferencia en MercadoPago
-      preference.create({
-          body: {
-            items: order_items.map(item => ({
-              title: item.product_sku.sku,
-              unit_price: item.product_sku.price,
-              quantity: item.quantity,
-              currency_id: "COP"
-            })),
+    const invoice = await prisma.invoice.create({
+      data: {
+        order_id: order.id,
+        amount: total,
+        status: "paid",
+        transaction_id: "test",
+        bank: "mercadopago",
+      },
+    });
 
-            back_urls: {
-              "success": `${URL_FRONT}/pago-realizado`,
-              "failure": `${URL_FRONT}/pago-realizado`,
-              "pending": `${URL_FRONT}/pago-realizado`
-            },
-            
-            // notification_url: `${URL_BACK}/orders/webhook`,
-            external_reference: invoice.order_id,
+    const preference = new Preference(client);
+
+    //crear preferencia en MercadoPago
+    preference
+      .create({
+        body: {
+          items: order_items.map((item) => ({
+            title: item.product_sku.sku,
+            unit_price: item.product_sku.price,
+            quantity: item.quantity,
+            currency_id: "COP",
+          })),
+
+          back_urls: {
+            success: `${URL_FRONT}/pago-realizado`,
+            failure: `${URL_FRONT}/pago-realizado`,
+            pending: `${URL_FRONT}/pago-realizado`,
           },
-        })
-        .then(async (response) => {
-          console.log(response);
-          
-          await prisma.invoice.update({
-            where: {
-              id: invoice.id
-            },
-            data: {
-              transaction_id: response.id
-            }
-          })
 
-          let ordered_items = [];
-          for (const item of order_items) {
-            await this.productSkuService.decreaseStock(item.id, item.quantity);
-            ordered_items.push(await this.orderItemService.create({
+          // notification_url: `${URL_BACK}/orders/webhook`,
+          external_reference: invoice.order_id,
+        },
+      })
+      .then(async (response) => {
+        console.log(response);
+
+        await prisma.invoice.update({
+          where: {
+            id: invoice.id,
+          },
+          data: {
+            transaction_id: response.id,
+          },
+        });
+
+        let ordered_items = [];
+        for (const item of order_items) {
+          await this.productSkuService.decreaseStock(item.id, item.quantity);
+          ordered_items.push(
+            await this.orderItemService.create({
               product_sku_id: item.product_sku_id,
               order_id: order.id,
               quantity: item.quantity,
-              price: item.product_sku.price
-            }));
-          }
-          
-          const orderWithItems = { ...order, ordered_items, transaction: response };
-          res.status(201).json(orderWithItems);
-        } )
-        .catch(err => {
-          console.log(err);
-          res.status(500).json({ msg: "Error creating preference" });
-        } );
-      
-    };
+              price: item.product_sku.price,
+            })
+          );
+        }
 
-    validateStock = async (orderItems) => {
-        for (const item of orderItems) {
-            const product = await this.productSkuService.findOne(item.product_sku_id);
-            if (!product || product.quantity < item.quantity) {
-                return false;
-            }
-        }
-        return true;
-    };
-    
-    async webhook(req, res) {
-      try {
-        const body = req.body;
-        if (body.topic !== null && body.topic !== undefined && body.topic === 'merchant_order') {
-          const merchantOrder = new MerchantOrder(client);
-          const segments = body.resource.split('/');
-          const ordenDePago = await merchantOrder.get({ merchantOrderId: segments[segments.length - 1] });
-          const status = ordenDePago.status;
-          const idFactura = ordenDePago.external_reference;
-          // const factura = await facturaService.findOne(idFactura);
-          
-          const factura = await prisma.invoice.findUnique({
-            where: {
-              id: idFactura
-            }
-          });
-          
-          console.log(factura)
-          console.log(status)
-        }
-        res.json({ success: true });
-      } catch (error) {
-        console.log(error);
-        res.status(500).send({ success: false, message: error.message });
+        const orderWithItems = {
+          ...order,
+          ordered_items,
+          transaction: response,
+        };
+        res.status(201).json(orderWithItems);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ msg: "Error creating preference" });
+      });
+  };
+
+  validateStock = async (orderItems) => {
+    for (const item of orderItems) {
+      const product = await this.productSkuService.findOne(item.product_sku_id);
+      if (!product || product.quantity < item.quantity) {
+        return false;
       }
     }
+    return true;
+  };
+
+  async webhook(req, res) {
+    try {
+      const body = req.body;
+      if (
+        body.topic !== null &&
+        body.topic !== undefined &&
+        body.topic === "merchant_order"
+      ) {
+        const merchantOrder = new MerchantOrder(client);
+        const segments = body.resource.split("/");
+        const ordenDePago = await merchantOrder.get({
+          merchantOrderId: segments[segments.length - 1],
+        });
+        const status = ordenDePago.status;
+        const idFactura = ordenDePago.external_reference;
+        // const factura = await facturaService.findOne(idFactura);
+
+        const factura = await prisma.invoice.findUnique({
+          where: {
+            id: idFactura,
+          },
+        });
+
+        console.log(factura);
+        console.log(status);
+      }
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ success: false, message: error.message });
+    }
+  }
 }
 
 export default OrderController;
--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -1,100 +1,101 @@
-import prisma from '../config/prisma.js';
+import prisma from "../config/prisma.js";
 
 class OrderService {
-    find = async (params) => {
-        const orders = await prisma.order.findMany({
-            ...params,
-            select: {
-                id: true,
-                status: true,
-                createdAt: true,
-                is_wholesale: true,
-                total: true,
-                user: {
-                    select: {
-                        firstName: true,
-                        email: true,
-                    },
+  find = async (where) => {
+    const orders = await prisma.order.findMany({
+      where: where,
+      select: {
+        id: true,
+        status: true,
+        createdAt: true,
+        is_wholesale: true,
+        total: true,
+        user: {
+          select: {
+            id: true,
+            firstName: true,
+            email: true,
+          },
+        },
+        address: {
+          select: {
+            title: true,
+            address_line_1: true,
+            address_line_2: true,
+            country: true,
+            city: true,
+            postal_code: true,
+          },
+        },
+        items: {
+          select: {
+            quantity: true,
+            price: true,
+            product_sku: {
+              select: {
+                size_attribute: true,
+                color_attribute: true,
+                product: {
+                  select: {
+                    name: true,
+                  },
                 },
-                address: {
-                    select: {
-                        title: true,
-                        address_line_1: true,
-                        address_line_2: true,
-                        country: true,
-                        city: true,
-                        postal_code: true,
-                    },
+                photos: {
+                  select: {
+                    value: true,
+                  },
                 },
-                items: {
-                    select: {
-                        quantity: true,
-                        price: true,
-                        product_sku: {
-                            select: {
-                                size_attribute: true,
-                                color_attribute: true,
-                                product: {
-                                    select: {
-                                        name: true,
-                                    },
-                                },
-                                photos: {
-                                    select: {
-                                        value: true,
-                                    },
-                                },
-                            },
-                        },
-                    },
-                }
-            }
-        });
-        return orders;
-    };
-
-    create = async (data) => {
-        const order = await prisma.order.create({
-            data: {
-                status: data.status,
-                is_wholesale: data.is_wholesale,
-                total: data.total,
-                user: {
-                    connect: {
-                        id: data.user_id,
-                    },
-                },
-                address: {
-                    connect: {
-                        id: data.address_id,
-                    },
-                },
+              },
             },
-            select: {
-                id: true,
-                status: true,
-                is_wholesale: true,
-                total: true,
-                user: {
-                    select: {
-                        firstName: true,
-                        email: true,
-                    },
-                },
-                address: {
-                    select: {
-                        title: true,
-                        address_line_1: true,
-                        address_line_2: true,
-                        country: true,
-                        city: true,
-                        postal_code: true,
-                    },
-                },
-            }
-        });
-        return order;
-    };
+          },
+        },
+      },
+    });
+    return orders;
+  };
+
+  create = async (data) => {
+    const order = await prisma.order.create({
+      data: {
+        status: data.status,
+        is_wholesale: data.is_wholesale,
+        total: data.total,
+        user: {
+          connect: {
+            id: data.user_id,
+          },
+        },
+        address: {
+          connect: {
+            id: data.address_id,
+          },
+        },
+      },
+      select: {
+        id: true,
+        status: true,
+        is_wholesale: true,
+        total: true,
+        user: {
+          select: {
+            firstName: true,
+            email: true,
+          },
+        },
+        address: {
+          select: {
+            title: true,
+            address_line_1: true,
+            address_line_2: true,
+            country: true,
+            city: true,
+            postal_code: true,
+          },
+        },
+      },
+    });
+    return order;
+  };
 }
 
 export default OrderService;
