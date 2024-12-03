2b0a1856dc02a84ad7f5ef6185ccb25a9c7ebcef
MauricioDDS <mauriciodidonato794@gmail.com>
2024-12-03 13:15:56 -0500

    fix: handle cart errors

--- src/services/CartProductService.js
+++ src/services/CartProductService.js
@@ -1,96 +1,101 @@
 import prisma from "../config/prisma.js";
 
 class CartProductService {
+  async find(where) {
+    try {
+      const cartProducts = await prisma.cart.findMany({
+        where,
 
-    async find(where) {
-        const cartProducts = await prisma.cart.findMany({
-            where,
-          
-          include: {
-            products: {
-              include: {
-                product_sku: {
-                  include: {
-                    product: true,
-                    color_attribute: true,
-                    size_attribute: true,
-                    photos: true
-                  }
-                }
-              }
-            }
-          }
-        });
-        
-        return cartProducts;
-    }
-
-    async findOne(id) {
-        const cartProduct = await prisma.cartProduct.findUnique({
-            where: {
-                id: id,
-            },
-        });
-
-        return cartProduct;
-    }
-
-    async create(data) {
-      const {
-        productId,
-        userId,
-        quantity
-      } = data;
-      
-        const cart = await prisma.cart.findFirst({
-            where: {
-                userId: userId,
-            },
-          })
-      
-        const cartProductCreated = await prisma.cartProduct.create({
-            data: {
-              quantity,
-              
+        include: {
+          products: {
+            include: {
               product_sku: {
-                connect: {
-                  id: productId
-                }
+                include: {
+                  product: true,
+                  color_attribute: true,
+                  size_attribute: true,
+                  photos: true,
+                },
               },
-              
-              cart: {
-                connect: {
-                  id: cart.id
-                }
-              }
-
             },
-        });
+          },
+        },
+      });
 
-        return cartProductCreated;
+      return cartProducts;
+    } catch (err) {
+      console.log(err);
+      console.log("Error obteniedno carrito de compras");
+      return;
     }
+  }
 
-    async delete(id, cartId) {
-        const cartProductDeleted = await prisma.cartProduct.delete({
-            where: {
-                id: id,
-            }
-        });
-        return cartProductDeleted;
-    }
+  async findOne(id) {
+    const cartProduct = await prisma.cartProduct.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-    async update(id, userId, quantity) {
-        const cartProductUpdated = await prisma.cartProduct.update({
-            where: {
-                id: id,
+    return cartProduct;
+  }
+
+  async create(data) {
+    const { productId, userId, quantity } = data;
+    try {
+      const cart = await prisma.cart.findFirst({
+        where: {
+          userId: userId,
+        },
+      });
+
+      const cartProductCreated = await prisma.cartProduct.create({
+        data: {
+          quantity,
+
+          product_sku: {
+            connect: {
+              id: productId,
             },
-            data: {
-                quantity: quantity,
-            },
-        });
+          },
 
-        return cartProductUpdated;
+          cart: {
+            connect: {
+              id: cart.id,
+            },
+          },
+        },
+      });
+
+      return cartProductCreated;
+    } catch (err) {
+      console.log(err);
+      console.log("Error agregando producto al carrito");
+      return null;
     }
+  }
+
+  async delete(id, cartId) {
+    const cartProductDeleted = await prisma.cartProduct.delete({
+      where: {
+        id: id,
+      },
+    });
+    return cartProductDeleted;
+  }
+
+  async update(id, userId, quantity) {
+    const cartProductUpdated = await prisma.cartProduct.update({
+      where: {
+        id: id,
+      },
+      data: {
+        quantity: quantity,
+      },
+    });
+
+    return cartProductUpdated;
+  }
 }
 
 export default CartProductService;
